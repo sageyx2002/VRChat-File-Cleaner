bd87fadcd13deb03e9dd436d54e2778dac6e31b3
Sage Skyler <104058571+sageyx2002@users.noreply.github.com>
2024-11-10 03:41:17 +0100

    Update run.bat
    Removed check for ToN Save Manager, this no longer exists.

--- run.bat
+++ run.bat
@@ -3,17 +3,12 @@ title VRC File Cleaner
 cls
 net session >nul 2>&1
 if %errorlevel% NEQ 0 (
-    echo This script needs to be executed with administrative permissions^!
+    echo This script needs to be granted administrative permissions^!
     pause >nul
     exit
 )
 rmdir /s /q "%LocalAppData%\Temp\VRChat" && echo Deleting Temporary Data... || echo An error occurred and the directory could not be deleted!
-if exist "%UserProfile%\AppData\LocalLow\VRChat\ToNSaveManager\" (
-    echo ToN Save Manager directory found! Deleting child directory instead.
-    rmdir /s /q "%UserProfile%\AppData\LocalLow\VRChat\VRChat" && echo Deleting Local User Data... || echo An error occurred and the directory could not be deleted!
-) else (
-    rmdir /s /q "%UserProfile%\AppData\LocalLow\VRChat" && echo Deleting Local User Data... || echo An error occurred and the directory could not be deleted!
-)
+rmdir /s /q "%UserProfile%\AppData\LocalLow\VRChat" && echo Deleting Local User Data... || echo An error occurred and the directory could not be deleted!
 REG DELETE "HKEY_CURRENT_USER\SOFTWARE\VRChat" /f && echo Deleting Registry Keys... || echo An error occurred and the Registry Keys could not be deleted!
 echo.
 echo Done!
